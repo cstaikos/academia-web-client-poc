f6c7f36e4e4433b861508b385561bc52918b5a7e
Chris Staikos <cstaikos@gmail.com>
2018-06-11 12:41:13 -0400

    Get basic store working

---
+++ src/components/ManuscriptPreview.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="manuscript-preview">
+    <span class="manuscript-title">{{ manuscript.title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'manuscript-preview',
+  props: {
+    manuscript: {type: Object, required: true}
+  }
+}
+</script>
+
+<style lang="scss">
+  .manuscript-preview {
+    .manuscript-title {
+      
+    }
+  }
+</style>
--- src/components/ManuscriptsList.vue
+++ src/components/ManuscriptsList.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="manuscripts-list">
-
+    <ManuscriptPreview
+    v-for="(manuscript, index) in manuscripts"
+    :manuscript="manuscript"
+    :key="index">
+    </ManuscriptPreview>
   </div>
 </template>
 
 <script>
+import Store from "../store"
+import ManuscriptPreview from "./ManuscriptPreview.vue"
+
 export default {
+  computed: {
+    manuscripts() { return this.$store.state.manuscripts }
+  },
+
   created() {
     console.log("list created!");
+    console.log(this.$store.state.manuscripts);
+  },
+
+  components: {
+    ManuscriptPreview
   }
 }
 </script>
